82856c758bb354898fdd4c804e29a7bd81792d68
dariusz szymczak <dariusz.szymczak96@gmail.com>
2019-10-20 16:37:30 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,8 +9,10 @@ app.use(express.static(__dirname + '/dist/projekt'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/projekt/index.html'));
+res.sendFile(path.join(__dirname+'/dist/projekt/index.html'));
+
 });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
